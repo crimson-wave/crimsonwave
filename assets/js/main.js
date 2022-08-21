dcb0df2afec29fa22e674ce9bb85efd67eb9eb16
fialo4ka <tona16@inbox.lv>
2022-08-21 21:13:55 +0200

    mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,29 @@
 // Script to open and close sidebar
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-  }
-   
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
 
-  
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+
+function open_submenu(type) {
+  if (type === "nav_submenu") {
+    let education = document.getElementById("education");
+    let navSubmenu = document.getElementById("nav_submenu");
+    if (navSubmenu.style.display === "none") {
+            education.innerHTML = 'Education <i class="fa fa-caret-right"></i>';
+            navSubmenu.style.display = "block";
+    }
+    else {
+            education.innerHTML = 'Education <i class="fa fa-caret-down"></i>';
+            navSubmenu.style.display = "none";
+    }
+  }
+  // if (type === "") {
+
+  // }
+}
